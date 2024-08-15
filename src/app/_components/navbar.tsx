df13651fa93e81c52ac4155dfcc0bd2d54529b42
plaseo <cameron@plaseo.org>
2024-08-15 10:41:51 -0500

    setup auth(Clerk) and create navbar component

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,18 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
+
+export function NavBar() {
+    return (
+      <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
+        <div>Gallery</div>
+
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          </div>
+      </nav>
+    )
+  }
