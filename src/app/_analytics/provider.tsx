3dcd845a88a2c80723df5d28313b5f03e67b2c8b
plaseo <cameron@plaseo.org>
2024-08-17 17:01:32 -0500

    small fix on analytics

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -7,15 +7,16 @@ import { useEffect } from "react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: "/ingest",
-    ui_host: 'https://us.posthog.com' 
+    ui_host: "https://us.posthog.com",
   });
 }
+
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return (
-        <PostHogProvider client={posthog}>
-            <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
-        </PostHogProvider>
-    );
+    <PostHogProvider client={posthog}>
+      <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+    </PostHogProvider>
+  );
 }
 
 function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
