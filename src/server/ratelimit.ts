9f1831623f373d113afb6b3cfd3a40691ebb23e8
plaseo <cameron@plaseo.org>
2024-08-17 22:11:25 -0500

    update rate limit

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(15, "100 s"),
+  limiter: Ratelimit.slidingWindow(50, "100 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
