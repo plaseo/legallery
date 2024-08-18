2e376de90fb2cc9bdf8000f7a0407830d97d4b8b
plaseo <cameron@plaseo.org>
2024-08-17 22:06:14 -0500

    lockdown uploads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { images } from "~/server/db/schema";
 import { db } from "~/server/db";
 import { ratelimit } from "~/server/ratelimit";
@@ -12,6 +12,10 @@ export const ourFileRouter = {
     .middleware(async ({ req }) => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+      if (fullUserData?.privateMetadata?.["can-upload"] !== "true") {
+        throw new UploadThingError("USER DOES NOT HAVE UPLOAD PERMISSION");
+      }
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("EXCEEDED RATE LIMIT");
       return { userId: user.userId };
